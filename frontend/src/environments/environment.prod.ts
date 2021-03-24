b2a4bdbd67ca31cefe7f7d824daef3151f31bc29
Himanshu Shekhar <hshekhar.dev@gmail.com>
2021-03-24 22:12:49 +0530

    fix prod build
    * add user to firestore on login
    
    * fix

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBOlT99kmQywHFABHs4rmzNMZACmxPuIFM",
+    authDomain: "bhayanak-streamer.firebaseapp.com",
+    projectId: "bhayanak-streamer",
+    storageBucket: "bhayanak-streamer.appspot.com",
+    messagingSenderId: "870104437611",
+    appId: "1:870104437611:web:8a4f42b850248274f0322e",
+    measurementId: "G-7B09948T96"
+  }
 };
